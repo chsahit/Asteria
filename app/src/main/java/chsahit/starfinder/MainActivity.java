84607c7ed263ac9706c044b9ddb0264fd356f276
sahit <chsahit@gmail.com>
2015-08-29 23:38:06 -0400

    fixed request, can obtain lat+long from device

--- app/src/main/java/chsahit/starfinder/MainActivity.java
+++ app/src/main/java/chsahit/starfinder/MainActivity.java
@@ -1,11 +1,20 @@
 package chsahit.starfinder;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,20 +24,43 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity
+        implements LocationListener,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    double longitude = 0.0;
+    double latitude = 0.0;
+    GoogleApiClient client;
+    LocationRequest request;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        client = new GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        client.connect();
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         try {
-            getNearestShootingStar(74, 40.7);
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        } catch (Exception e) {
+            //while(longitude==0.0)
+            try {
+                Log.w("starfinder", "sleep");
+                Thread.sleep(5000);
+                Log.w("starfinder","end sleep");
+                //Log.w("starfinder",longitude+"");
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+        }
+        //Log.w("starfinder",longitude+"");
+        try {
+            getNearestShootingStar(longitude, latitude);
         } catch (Exception e) {/*darn*/}
     }
 
@@ -66,26 +98,62 @@ public class MainActivity extends ActionBarActivity {
         //api endpoint we are posting to w/params preformatted
         String address = "https://communities.socrata.com/resource/27eq-bd6d.json" +
                 "?$order=distance_in_meters(%22POINT%20(" + longitude + "%20" + lat + ")%22,%20geolocation) ASC" +
-                "&$limit=1";
+                "&$limit=10&&$$app_token=4SmTmWiVWkhE7W839XKUeHurc";
+        address = "https://communities.socrata.com/resource/27eq-bd6d.json?$order=distance_in_meters(%22POINT%20(" + longitude + "%20" + lat + ")%22,%20geolocation)%20ASC&$limit=10&&$$app_token=4SmTmWiVWkhE7W839XKUeHurc";
+        //address = "https://google.com";
+        //String address = "https://google.com";
         String response = ""; //response contains the json
-        Log.w("starfinder", address);
+        //Log.w("starfinder", address);
         try {
             HttpURLConnection urlConnection = (HttpURLConnection)
                     new URL(address).openConnection(); //make request
-
+            /**urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(30*1000);
+            urlConnection.setDoInput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.connect();**/
+            Log.w("starfinder",urlConnection.getResponseMessage());
             InputStream is = new BufferedInputStream(urlConnection.getInputStream());
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
             String line = "";
+            Log.w("starfinder","startingloop");
             while ((line = rd.readLine()) != null) {
                 response += line;
+                Log.w("starfinder","reading");
             }
             rd.close();
             is.close();
-            Log.w("starfinder",response + " is response");
+            Log.w("starfinder", response + " is response");
         } catch (Exception e) {
             //cry
-            Log.w("starfinder",e.getMessage());
+            Log.w("starfinder",e.getMessage() + " was an error");
         }
         return new JSONObject(response); //return JSON
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.w("starfinder","connected to API");
+        request = new LocationRequest();
+        request.setInterval(1000);
+        request.setFastestInterval(500);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationServices.FusedLocationApi.requestLocationUpdates(client, request, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
